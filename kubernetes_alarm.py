11216705f7cd2413bfb17cb09512d5f17542e041
karoo <gksmf200182@gmail.com>
2023-09-01 21:59:55 +0900

    async def renewal

--- kubernetes_alarm.py
+++ kubernetes_alarm.py
@@ -8,7 +8,8 @@ from aiogram.types import ReplyKeyboardMarkup
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.base import JobLookupError
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+#asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 #stble node list
 k8s_nodes_lists = []
@@ -20,37 +21,57 @@ id = 5741333087
 dp = Dispatcher(bot)
 api_client = None
 
+status_color = {
+    'Green' : '🟢',
+    'Red' : '🔴'
+}
+
 keyboard_reply = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True).add("Node_Status", "Pod_Check")
 
 async def err():
     await bot.send_message(id, "Kubernetes is Down\nConnection Failed")
 
+#@dp.message_handler()
 async def botsend(message: types.message):
     await bot.send_message(id, message)
 
+#@dp.message_handler()
 async def errsend(message: types.message, reason: types.message):
-    await bot.send_message(id, message + "\nReason : " + reason)
+    await bot.send_message(id, message + "\n" + reason)
+
 
 def check_nodes():
     global k8s_nodes_lists, k8s_nodes_status
     err_node = []
+    print(k8s_nodes_lists)
+    print(k8s_nodes_status)
+
+    # if (k8s_nodes_lists == []):
+    #     print("k8s_nodes_list is Empty")
     
     for i in k8s_nodes_lists:
         if any(i[0] == x[0] for x in k8s_nodes_status):
             continue
         err_node.append(i[0])
-        print(i[0] + "is down")
+        print("Node : " + i[0] + " is down")
     
     if len(err_node) != 0:
         return err_node
     elif len(err_node) == 0:
         return None
 
+
 @dp.message_handler(commands=['start', 'help'])
 async def welcome(message: types.Message):
-    # Sending a greeting message that includes the reply keyboard
-    await message.reply("Hello! how are you?", reply_markup=keyboard_reply)
+    await conn_k8s()
+    await bot.send_message(id, "K8S Cluster Connect! Start bot!", reply_markup=keyboard_reply)
+    await update_nodes_list('update')
+    schedule_node = AsyncIOScheduler()
+    schedule_node.add_job(schedule_node_check, 'cron', second="*/10")
+    schedule_node.start()
+    #await message.reply("Hello! how are you?", reply_markup=keyboard_reply)
+
 
 @dp.message_handler(commands=['update'])
 async def update_nodes_list(message: types.Message):
@@ -63,6 +84,8 @@ async def update_nodes_list(message: types.Message):
             node_cpu_usage = (str)((int)(stats['usage']['cpu'][:-1])//1000000)+"m"
             node_memory_usage = (int)(stats['usage']['memory'][:-2])//1000
             k8s_nodes_lists.append([node_name, node_cpu_usage, node_memory_usage])
+    print("update success")
+
 
 @dp.message_handler()
 async def check_rp(message: types.Message):
@@ -71,27 +94,27 @@ async def check_rp(message: types.Message):
     k8s_nodes_status = []
     message_txt = ""
     if message.text == 'Node_Status':
-        #k8s_nodes_status = "----------- Lawdians Internal K8S -----------\n"
+        message_txt = "------------ Lawdians Internal K8S ------------\n"
         # Responding with a message for the first button
         k8s_nodes = top.list_cluster_custom_object("metrics.k8s.io","v1beta1", "nodes")
         for stats in k8s_nodes['items']:
             node_name = stats['metadata']['name'][3:]
             node_cpu_usage = (str)((int)(stats['usage']['cpu'][:-1])//1000000)+"m"
-            node_memory_usage = (int)(stats['usage']['memory'][:-2])//1000
+            node_memory_usage = (str)((int)(stats['usage']['memory'][:-2])//1000)+"Mi"
             k8s_nodes_status.append([node_name, node_cpu_usage, node_memory_usage])
-            message_txt = message_txt + f"{node_name:<12} CPU: {node_cpu_usage:>4}   Mem: {node_memory_usage:>5}Mi\n"
-        
-        print(k8s_nodes_status)
+            message_txt = message_txt + f"{status_color['Green']}  {node_name:<11s} CPU: {node_cpu_usage:>4s} Mem: {node_memory_usage:>7s}\n"
 
         err_nodes = check_nodes()
+
         if err_nodes == None:
-            await bot.send_message(message.from_user.id, message_txt)
+            await bot.send_message(id, message_txt)
         elif err_nodes != None:
-            message_txt = message_txt + "\n----------------err node----------------\nn"
+            #message_txt = message_txt + "\n----------------err node----------------\nn"
             for i in err_nodes:
                 print(i)
-                message_txt += (i + " is Down")   
-            await bot.send_message(message.from_user.id, message_txt)
+                #message_txt += (i + " is Down")   
+                message_txt = message_txt + f"{status_color['Red']}  {i:<11s} CPU:  ??m Mem:  ????Mi\n"
+            await bot.send_message(id, message_txt)
 
     elif message.text == 'Pod_Check':
         k8s_nodes_lists = ""
@@ -108,13 +131,17 @@ async def check_rp(message: types.Message):
             elif running == None and terminated == None:
                 k8s_nodes_lists += f"{namespace}  {pod_name}  "+ info.status.container_statuses[0].state.waiting.reason + "\n"
         await bot.send_message(message.from_user.id, running_pods_num)
-# error_pod_num = "np           pod                  status\n"
 
-def schedule_node_check():
+
+async def schedule_node_check():
+    print("schedule_node_ckeck start")
     global k8s_nodes_status
     k8s_nodes_status=[]
-    top = client.CustomObjectsApi()
-    k8s_nodes = top.list_cluster_custom_object("metrics.k8s.io","v1beta1", "nodes")
+    try:
+        top = client.CustomObjectsApi()
+        k8s_nodes = top.list_cluster_custom_object("metrics.k8s.io","v1beta1", "nodes")
+    except:
+        await err()
     
     for stats in k8s_nodes['items']:
         node_name = stats['metadata']['name'][3:]
@@ -125,20 +152,19 @@ def schedule_node_check():
     err_nodes = check_nodes()
     message_txt =""
     if err_nodes != None:
-        message_txt = message_txt + "\n----------------err node----------------\nn"
+        #message_txt = message_txt + "\n----------------err node----------------\nn"
         for i in err_nodes:
             print(i)
-            message_txt += (i + " is Down\n")
-        try:
-            asyncio.run(errsend("Node Down Detect",message_txt))
-        except:
-            print("error")
-        asyncio.run(err())
+            message_txt = message_txt + f"{status_color['Red']}  {i:<11s}         is UNKNOWN        \n"
+        await errsend("--------------🚨 Node Down Detect 🚨--------------", message_txt)
+
     elif err_nodes == None:
         print("schedule_node_check_Report : Stable")
 
-def conn_k8s():
+
+async def conn_k8s():
     global api_client
+    print(k8s_nodes_lists)
     while True:
         try:
             print("try connect")
@@ -151,30 +177,27 @@ def conn_k8s():
             break 
         except:
             print("kubernetes Cluster Connection failed.")
-            try:
-                asyncio.run(err())
-            except:
-                print("error")
-            time.sleep(300)
+            await errsend("K8S Cluster Connect Error", "Kube-config is not Enable")
+            time.sleep(10)
 
 def main():
     conn_k8s()
+    #asyncio.run(update_nodes_list('update'))
+    sched = BackgroundScheduler()
+    sched.start()
+    sched.add_job(conn_k8s, 'cron', minute="*/5", id="conn_test")
 
-    # sched = BackgroundScheduler()
-    # sched.start()
-    # sched.add_job(conn_k8s, 'cron', minute="*/5", id="conn_test")
+    alarm_node_status = BackgroundScheduler()
+    alarm_node_status.start()
+    alarm_node_status.add_job(schedule_node_check, 'cron', second="*/5", id="node_status_checking")
 
-    # alarm_node_status = BackgroundScheduler()
-    # alarm_node_status.start()
-    # alarm_node_status.add_job(schedule_node_check, 'cron', second="*/10", id="node_status_checking")
-
-    executor.start_polling(dp)
-
-    # try:
-    #     executor.start_polling(dp)
-    # except:
-    #     print("errrrrrr")
-    #     time.sleep(30000)
+    try:
+        executor.start_polling(dp)
+    except:
+        print("Bot error")
     
 if __name__=="__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    loop.create_task(dp.start_polling())
+    
+    loop.run_forever()
